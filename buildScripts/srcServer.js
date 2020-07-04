6cf7f9ff1b2009423771864197021eda08a1323e
michael-basweti <baswetima@gmail.com>
2020-07-04 17:46:43 +0300

    create endpoint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,9 +15,19 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath:config.output.publicPath
 }));
 
+
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'))
 })
+
+app.get('/users', function(req, res){
+  res.json([
+    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bobsmith@gmail.com"},
+    {"id":2,"firstName":"Bob1","lastName":"Smith1","email":"bobsmith1@gmail.com"},
+    {"id":3,"firstName":"Bob2","lastName":"Smith2","email":"bobsmith2@gmail.com"}
+  ]);
+});
 
 app.listen(port, function(err){
   if(err){
